d8ec074247ec16da604f4c473113b847e00e91c9
Sam Inloes <sam@featureform.com>
2021-11-09 19:50:53 -0500

    use example rpc as basic latency test

---
+++ run-service.sh
@@ -0,0 +1,1 @@
+cd service && docker build -t go-service -f Dockerfile.service . && docker run -p 2112:2112 -d go-service
---
+++ run.sh
@@ -0,0 +1,1 @@
+cd prometheus && docker build -t my-prometheus -f Dockerfile.prom . && docker run -p 9090:9090 -d my-prometheus
